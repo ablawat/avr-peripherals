38cba1c849e8a959ad1b8bce530dbf6dd67b272d
Adam Bławat <blawat.adam@gmail.com>
2024-11-17 12:31:52 +0100

    Add Clock Control Configuration Module
    * Created register definitions and configuration options constants.
    * Created module configuration settings file.
    * Implemented configuration registers values calculation.
    * Implemented 'clkctrl_init' subroutine.

---
+++ clkctrl/clkctrl.asm
@@ -0,0 +1,38 @@
+.INCLUDE "clkctrl/clkctrl-config.inc"
+
+;***************************************************************************************************
+; @brief    Initializes Clock Control Peripheral
+;
+; @input    : none
+; @output   : none
+;
+; @used     : XH:XL (changed), r16 (changed)
+;***************************************************************************************************
+clkctrl_init:   ; get clock controller base address
+                ldi     XH, HIGH (CLKCTRL_base)         ;
+                ldi     XL, LOW  (CLKCTRL_base)         ;
+
+                ldi     r17, CPU_CCP_IOREG_gc           ; get I/O registers unlock value
+
+                ; unlock protected I/O registers
+                out     CPU_CCP, r17                    ; write into configuration change protection
+
+                ; configure register MCLKCTRLA
+                ldi     r16, CONFIG_CLKCTRL_MCLKCTRLA   ; get MCLKCTRLA config
+                st      X+, r16                         ; write into MCLKCTRLA
+
+                ; unlock protected I/O registers
+                out     CPU_CCP, r17                    ; write into configuration change protection
+
+                ; configure register MCLKCTRLB
+                ldi     r16, CONFIG_CLKCTRL_MCLKCTRLB   ; get MCLKCTRLB config
+                st      X+, r16                         ; write into MCLKCTRLB
+
+                ; unlock protected I/O registers
+                out     CPU_CCP, r17                    ; write into configuration change protection
+
+                ; configure register MCLKLOCK
+                ldi     r16, CONFIG_CLKCTRL_MCLKLOCK    ; get MCLKLOCK config
+                st      X+, r16                         ; write into MCLKLOCK
+
+                ret
