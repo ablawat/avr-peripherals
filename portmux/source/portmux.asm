bbd72ce8dd91f66e7c07216794c905a30ccb178e
Adam Bławat <blawat.adam@gmail.com>
2025-11-16 01:04:59 +0100

    Apply New Structure for Port Multiplexer
    * Renamed module configuration files according to new convention.
    * Changed definitions of configuration values for registers.
    * Added better comments and descriptions in configuration file.

---
+++ portmux/source/portmux.asm
@@ -0,0 +1,33 @@
+;**********************************************************************************************;
+; @section : Global Subroutine                                                                 ;
+;**********************************************************************************************;
+
+;**********************************************************************************************;
+; @brief    Initializes Port Multiplexer Peripheral
+;
+; @input    : none
+; @output   : none
+;
+; @used     : XH:XL (changed), TEMPL (changed)
+;**********************************************************************************************;
+portmux_init:   ; get port multiplexer base address
+                ldi     XH, HIGH (PORTMUX_base)
+                ldi     XL, LOW  (PORTMUX_base)
+
+                ; configure register CTRLA
+                ldi     TEMPL, CONFIG_PORTMUX_CTRLA     ; get config constant
+                st      X+, TEMPL                       ; write into register
+
+                ; configure register CTRLB
+                ldi     TEMPL, CONFIG_PORTMUX_CTRLB     ; get config constant
+                st      X+, TEMPL                       ; write into register
+
+                ; configure register CTRLC
+                ldi     TEMPL, CONFIG_PORTMUX_CTRLC     ; get config constant
+                st      X+, TEMPL                       ; write into register
+
+                ; configure register CTRLD
+                ldi     TEMPL, CONFIG_PORTMUX_CTRLD     ; get config constant
+                st      X+, TEMPL                       ; write into register
+
+                ret
