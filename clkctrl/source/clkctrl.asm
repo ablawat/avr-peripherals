7cafb3d71212b4332e9ec9a3822a2cc3550cc8f6
Adam Bławat <blawat.adam@gmail.com>
2025-12-19 00:29:02 +0100

    Update Source Template for Clock Configuration
    * Changed block comments inside source files.
    * Replaced local macros with reference to global macros.

--- clkctrl/source/clkctrl.asm
+++ clkctrl/source/clkctrl.asm
@@ -1,42 +1,36 @@
-;***********************************************************************************************************************
-;** Include Files                                                                                                     **
-;***********************************************************************************************************************
-
-.INCLUDE "clkctrl/source/clkctrl.inc"
-
-;***********************************************************************************************************************
+;**********************************************************************************************;
 ; @brief    Initializes Clock Controller Peripheral
 ;
 ; @input    : none
 ; @output   : none
 ;
-; @used     : XH:XL (changed), r16 (changed)
-;***********************************************************************************************************************
+; @used     : XH:XL (changed), TEMPH (changed), TEMPL (changed)
+;**********************************************************************************************;
 clkctrl_init:   ; get clock controller base address
-                ldi     XH, HIGH (CLKCTRL_base)         ;
-                ldi     XL, LOW  (CLKCTRL_base)         ;
+                ldi     XH, HIGH (CLKCTRL_base)
+                ldi     XL, LOW  (CLKCTRL_base)
 
-                ldi     r17, CPU_CCP_IOREG_gc           ; get I/O registers unlock value
+                ldi     TEMPH, CPU_CCP_IOREG_gc         ; get I/O registers unlock value
 
                 ; unlock protected I/O registers
-                out     CPU_CCP, r17                    ; write into configuration change protection
+                out     CPU_CCP, TEMPH                  ; write into configuration change protection
 
                 ; configure register MCLKCTRLA
-                ldi     r16, CONFIG_CLKCTRL_MCLKCTRLA   ; get MCLKCTRLA config
-                st      X+, r16                         ; write into MCLKCTRLA
+                ldi     TEMPL, CONFIG_CLKCTRL_MCLKCTRLA ; get config constant
+                st      X+, TEMPL                       ; write into register
 
                 ; unlock protected I/O registers
-                out     CPU_CCP, r17                    ; write into configuration change protection
+                out     CPU_CCP, TEMPH                  ; write into configuration change protection
 
                 ; configure register MCLKCTRLB
-                ldi     r16, CONFIG_CLKCTRL_MCLKCTRLB   ; get MCLKCTRLB config
-                st      X+, r16                         ; write into MCLKCTRLB
+                ldi     TEMPL, CONFIG_CLKCTRL_MCLKCTRLB ; get config constant
+                st      X+, TEMPL                       ; write into register
 
                 ; unlock protected I/O registers
-                out     CPU_CCP, r17                    ; write into configuration change protection
+                out     CPU_CCP, TEMPH                  ; write into configuration change protection
 
                 ; configure register MCLKLOCK
-                ldi     r16, CONFIG_CLKCTRL_MCLKLOCK    ; get MCLKLOCK config
-                st      X+, r16                         ; write into MCLKLOCK
+                ldi     TEMPL, CONFIG_CLKCTRL_MCLKLOCK  ; get config constant
+                st      X+, TEMPL                       ; write into register
 
                 ret
